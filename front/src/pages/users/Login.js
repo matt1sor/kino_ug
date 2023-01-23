33a7cba33eb053cfc5b75e7c1669d75bf166a8c0
matt1sor <mattidepka@gmail.com>
2023-01-23 19:30:43 +0100

    deleted comments and changed text

--- front/src/pages/users/Login.js
+++ front/src/pages/users/Login.js
@@ -14,6 +14,7 @@ import {
   FormLabel,
   Input,
   Link,
+  Text,
   VStack,
 } from "@chakra-ui/react";
 
@@ -32,110 +33,76 @@ function Login() {
     login: Yup.string().max(50, "Login is too long!").required(),
     password: Yup.string().max(100, "Wrong password!").required(),
   });
-  // const submitForm = async (values, actions) => {
-  //   setLoading(true);
-  //   dispatch(loginHandler(values));
-  //   actions.resetForm();
-  //   navigate("/repertoire");
-  // };
-
-  // const formik = useFormik({
-  //   initialValues: {
-  //     login: "",
-  //     password: "",
-  //   },
-  //   validationSchema: Yup.object({
-  //     login: Yup.string().max(50, "Login is too long!").required(),
-  //     password: Yup.string().max(100, "Wrong password!").required(),
-  //   }),
-  //   onSubmit: async (values) => {
-  //     setLoading(true);
-  //     dispatch(loginHandler(values));
-  //     formik.resetForm();
-  //     navigate("/repertoire");
-  //   },
-  // });
 
   return (
     <Flex bg="gray.100" align="center" justify="center" h="100vh">
-      <Box bg="white" p={6} rounded="md">
-        <Formik
-          initialValues={initialValues}
-          onSubmit={(values) => {
-            //setLoading(true);
-            dispatch(loginHandler(values));
+      <VStack>
+        <Text fontSize="5xl" m={10}>
+          Welcome in Colleage Cinema
+        </Text>
+        <Box bg="white" p={6} rounded="md">
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(values) => {
+              //setLoading(true);
+              dispatch(loginHandler(values));
 
-            navigate("/repertoire");
-          }}
-          validationSchema={validationSchema}
-        >
-          {(formik) => (
-            <Form onSubmit={formik.handleSubmit}>
-              <VStack spacing={4} align="flex-start">
-                <FormControl
-                  isInvalid={formik.errors.login && formik.touched.login}
-                >
-                  <FormLabel htmlFor="login">Login</FormLabel>
-                  <Field
-                    as={Input}
-                    id="login"
-                    name="login"
-                    type="text"
-                    variant="filled"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                  />
-                  <FormErrorMessage>{formik.errors.login}</FormErrorMessage>
-                </FormControl>
-                <FormControl
-                  isInvalid={formik.errors.password && formik.touched.password}
-                >
-                  <FormLabel htmlFor="password">Password</FormLabel>
-                  <Field
-                    as={Input}
-                    id="password"
-                    name="password"
-                    type="password"
-                    variant="filled"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                  />
-                  <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-                </FormControl>
+              navigate("/repertoire");
+            }}
+            validationSchema={validationSchema}
+          >
+            {(formik) => (
+              <Form onSubmit={formik.handleSubmit}>
+                <VStack spacing={4} align="flex-start">
+                  <FormControl
+                    isInvalid={formik.errors.login && formik.touched.login}
+                  >
+                    <FormLabel htmlFor="login">Login</FormLabel>
+                    <Field
+                      as={Input}
+                      id="login"
+                      name="login"
+                      type="text"
+                      variant="filled"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                    />
+                    <FormErrorMessage>{formik.errors.login}</FormErrorMessage>
+                  </FormControl>
+                  <FormControl
+                    isInvalid={
+                      formik.errors.password && formik.touched.password
+                    }
+                  >
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Field
+                      as={Input}
+                      id="password"
+                      name="password"
+                      type="password"
+                      variant="filled"
+                      onChange={formik.handleChange}
+                      value={formik.values.password}
+                    />
+                    <FormErrorMessage>
+                      {formik.errors.password}
+                    </FormErrorMessage>
+                  </FormControl>
 
-                <Button type="submit" colorScheme="blue" width="full">
-                  Login
-                </Button>
-              </VStack>
-            </Form>
-          )}
-        </Formik>
+                  <Button type="submit" colorScheme="blue" width="full">
+                    Login
+                  </Button>
+                </VStack>
+              </Form>
+            )}
+          </Formik>
 
-        <Link as={ReachLink} to="/users/register">
-          Nie masz konta? Zarejestruj sie!
-        </Link>
-      </Box>
+          <Link as={ReachLink} to="/users/register">
+            Don't have an account?! Sign up now!
+          </Link>
+        </Box>
+      </VStack>
     </Flex>
-
-    // <Fragment>
-    //   {loading && <span>Loading...</span>}
-    //   <form onSubmit={formik.handleSubmit}>
-    //     <input
-    //       id="login"
-    //       name="login"
-    //       type="text"
-    //       onChange={formik.handleChange}
-    //     />
-    //     <input
-    //       id="password"
-    //       name="password"
-    //       type="password"
-    //       onChange={formik.handleChange}
-    //     />
-    //     <button type="submit">Submit</button>
-    //   </form>
-    //   <Link to="/users/register">Nie masz konta? Zarejestruj sie!</Link>
-    // </Fragment>
   );
 }
 
