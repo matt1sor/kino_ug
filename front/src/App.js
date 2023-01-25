e148eeead9bec922e44b314a23b76738badb0e08
matt1sor <mattidepka@gmail.com>
2023-01-25 02:28:33 +0100

    added initialize to app to keep logged in session

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,14 @@
 import Mainroutes from "./routes";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { initialize } from "./store/features/auth";
 
 function App() {
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(initialize());
+  // }, []);
+
   return <Mainroutes />;
 }
 
