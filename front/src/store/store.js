5018ce5cb9c497afb60b0ee484952a62d23b55a0
matt1sor <mattidepka@gmail.com>
2023-01-25 14:52:51 +0100

    added logger in store

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -3,12 +3,13 @@ import authReducer from "../store/features/auth";
 import repertoireReducer from "../store/features/repertoire";
 import movieReducer from "../store/features/movies";
 import logger from "redux-logger";
+import { applyMiddleware } from "redux";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     repertoire: repertoireReducer,
     movie: movieReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    middleware: applyMiddleware(logger),
   },
 });
